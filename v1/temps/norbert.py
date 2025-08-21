77f6e3c5e0518d48c0b27216f6ea814ea4027529
Sigurd Brinch <sigurd.k.brinch@uia.no>
2025-08-21 10:45:07 +0000

    data description in utils

--- v1/temps/norbert.py
+++ v1/temps/norbert.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import torch
+from transformers import AutoTokenizer, AutoModel
+
+# Import model
+tokenizer = AutoTokenizer.from_pretrained(
+    "ltg/norbert4-large"
+)
 
 df = pd.read_csv('../data/stortinget_dataset.csv.gz')
 
-print(df.head(5))
+print(df.columns)
---
+++ v1/utils/.ipynb_checkpoints/data_description-checkpoint.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+df = pd.read_csv('../data/stortinget_dataset.csv.gz')
+
+print('No Sammendrag & Vedtak, Only Text :',df[df[['sammendrag', 'vedtak']].isna().all(axis=1) & df['text'].notna()].shape[0])
+print('No Text & Vedtak, Only Sammendrag :',df[df[['text', 'vedtak']].isna().all(axis=1) & df['sammendrag'].notna()].shape[0])
+print('No Text & Sammendrag, Only Vedtak :',df[df[['text', 'sammendrag']].isna().all(axis=1) & df['vedtak'].notna()].shape[0])
+print('No Sammendrag, Vedtak, Text :',df[df[['sammendrag', 'vedtak', 'text']].isna().all(axis=1)].shape[0])
+print('No Sammendrag, Only Text & Vedtak :',df[df[['text', 'vedtak']].notna().all(axis=1) & df['text'].isna()].shape[0])
+print('No Vedtak, Only Text & Sammendrag :',df[df[['text', 'sammendrag']].notna().all(axis=1) & df['vedtak'].isna()].shape[0])
+print('No Text, Only Vedtak & Sammendrag :',df[df[['vedtak', 'sammendrag']].notna().all(axis=1) & df['text'].isna()].shape[0])
+
+print('Total:', df.shape[0])
+
+
+'''
+No Sammendrag & Vedtak, Only Text : 1
+No Text & Vedtak, Only Sammendrag : 0
+No Text & Sammendrag, Only Vedtak : 0
+No Sammendrag, Vedtak, Text : 3951
+No Sammendrag, Only Text & Vedtak : 0
+No Vedtak, Only Text & Sammendrag : 6157
+No Text, Only Vedtak & Sammendrag : 0
+Total: 17221
+
+'''
